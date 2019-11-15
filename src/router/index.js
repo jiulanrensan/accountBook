45150c801417f7cd7009ba2c033224d9d7859634
lijiacheng <839492148@qq.com>
2019-11-15 17:21:54 +0800

    初始化页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   redirect: '/home'
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: resolve => require(['@/pages/home'], resolve)
     }
   ]
 })
