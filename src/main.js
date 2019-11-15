83094e3e744fb7c9496c9e9e2038c79987335d07
lijiacheng <839492148@qq.com>
2019-11-15 18:12:42 +0800

    2019年11月15日18:12:41

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import { NavBar } from 'pagoda-mobile'
+
+
 
 Vue.config.productionTip = false
+
+Vue.use(NavBar)
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>首页</div>
+  <pagoda-nav-bar
+    :title="title"
+    :left-text="leftText"
+    :left-arrow="arrowShow"
+    fixed
+    @click-left="onClickLeft"
+    @click-right="onClickRight"
+  >
+    <pagoda-icon name="search" slot="right" />
+  </pagoda-nav-bar>
 </template>
 
 <script>
 export default {
-  name: 'home'
+  name: 'home',
+  data () {
+    return {
+      leftText: '返回',
+      arrowShow: false,
+      title: '记账本'
+    }
+  },
+  methods: {
+    onClickLeft () {},
+    onClickRight () {}
+  }
 }
 </script>
 
