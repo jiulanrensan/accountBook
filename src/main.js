91d57b2110cc190d02d8cc6a90f99e8eca087136
lijiacheng <839492148@qq.com>
2019-12-11 20:44:42 +0800

    组件全局引入

--- src/main.js
+++ src/main.js
@@ -3,10 +3,17 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import { NavBar, Icon, Popup, Row, Col, Button, Tag, List, Picker, Field, RadioGroup, Radio, Cell, CellGroup, Dialog, Checkbox, CheckboxGroup } from 'pagoda-mobile'
 Vue.config.productionTip = false
 
-Vue.use(NavBar).use(Icon).use(Popup).use(Row).use(Col).use(Button).use(Tag).use(List).use(Picker).use(Field).use(RadioGroup).use(Radio).use(Cell).use(CellGroup).use(Dialog).use(Checkbox).use(CheckboxGroup)
+// import { NavBar, Icon, Popup, Row, Col, Button, Tag, List, Picker, Field, RadioGroup, Radio, Cell, CellGroup, Dialog, Checkbox, CheckboxGroup } from 'pagoda-mobile'
+
+import 'pagoda-mobile/lib/index.css';
+import Pagoda from 'pagoda-mobile';
+Vue.use(Pagoda)
+
+
+
+// Vue.use(NavBar).use(Icon).use(Popup).use(Row).use(Col).use(Button).use(Tag).use(List).use(Picker).use(Field).use(RadioGroup).use(Radio).use(Cell).use(CellGroup).use(Dialog).use(Checkbox).use(CheckboxGroup)
 Vue.prototype.$EventBus = new Vue()
 /* eslint-disable no-new */
 new Vue({
